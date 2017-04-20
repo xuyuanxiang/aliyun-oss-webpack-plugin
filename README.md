7582a83d362bbdfdc5ef2bcafe4e39164aedfde9
徐远翔 <xuyuanxiang@wosai-inc.com>
2017-04-20 10:54:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# aliyun-oss-webpack-plugin
+
+`webpack`插件，用于将静态资源打包上传阿里云OSS。
+
+上传失败则终止`webpack`构建流程，为解决Docker构建中，上传失败无感的问题。
+
+## 安装
+
+```npm
+npm install aliyun-oss-webpack-plugin --save
+```
+
+## 使用
+
+`webpack`配置文件：
+
+```javascript
+var AliyunOSSPlugin = require("aliyun-oss-webpack-plugin");
+module.exports = {
+    output: {
+        publicPath: "https://domain.com/p/a/t/h" // 域名或域名+路径
+    },
+    plugins: [
+        // 其他插件
+        new AliyunOSSPlugin({
+            accessKeyId: '*****',
+            accessKeySecret: '*****',
+            region: 'oss-cn-hangzhou',
+            bucket: '*****'
+        })
+        // 其他插件
+    ]
+}
+```
---
+++ index.js
@@ -0,0 +1,54 @@
+/**
+ * @module
+ * @description
+ * @version 1.0.0
+ * @author xuyuanxiang
+ * @date 2017/1/6
+ */
+var url = require('url');
+var fs = require('fs');
+var _ = require('lodash');
+var async = require('async');
+var createOSS = require('ali-oss');
+var co = require('co');
+
+function AliyunOSSPlugin(options) {
+    if (!_.isPlainObject(options)
+        || _.isEmpty(options)
+        || _.isEmpty(options.region)
+        || _.isEmpty(options.accessKeyId)
+        || _.isEmpty(options.accessKeySecret)
+        || _.isEmpty(options.bucket)) {
+        throw new TypeError("缺少参数！");
+    }
+    this.oss = createOSS({
+        region: options.region,
+        accessKeyId: options.accessKeyId,
+        accessKeySecret: options.accessKeySecret,
+        bucket: options.bucket
+    });
+}
+
+AliyunOSSPlugin.prototype.apply = function (compiler) {
+    compiler.plugin('emit', function (compilation, callback) {
+        var publicPath = url.parse(compiler.options.output.publicPath);
+        if (!publicPath.protocol || !publicPath.hostname) {
+            return callback(new Error('Webpack配置文件中: "output.publicPath"必须设置为域名，例如： https://domain.com/p/a/t/h'));
+        }
+        async.every(_.filter(_.keysIn(compilation.assets), function (asset) {
+            return !/\.html$/.test(asset);
+        }), function (file, done) {
+            var target = url.resolve(url.format(publicPath), file);
+            var key = url.parse(target).pathname;
+            var source = compilation.assets[file].source();
+            var body = Buffer.isBuffer(source) ? source : new Buffer(source, 'utf8');
+            co(function *() {
+                yield this.oss.put(key, body);
+                console.log(file + '上传成功：', target);
+                done();
+            }.bind(this)).catch(callback);
+        }.bind(this), callback);
+    }.bind(this));
+}
+
+module.exports = AliyunOSSPlugin;
